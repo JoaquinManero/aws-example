6fc87e754365f917ce63bb97d154f73a5304d089
jmanero <ingmanerojoaquin@gmail.com>
2023-12-07 23:41:57 -0300

    cambio en server.js texto modificado

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World with express!");
+  res.send('<h1 style="color:blue">Hello World!</h1>');
 });
 
 app.listen(3000, () => {
