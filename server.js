416adb52c6f37719e7df54b1191f8b9bb4db6b8f
jmanero <ingmanerojoaquin@gmail.com>
2023-12-07 23:46:49 -0300

    cambio de puerto para probar

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.get("/", (req, res) => {
   res.send('<h1 style="color:blue">Hello World!</h1>');
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
